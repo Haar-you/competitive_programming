6fab678cb42c0bb3d3f916cbc33d1a8200130b16
Haar-you <zemlya.pole@gmail.com>
2018-08-16 13:19:16 +0900

    add unionfind.hs

---
+++ misc/unionfind.hs
@@ -0,0 +1,41 @@
+import Data.Array.IO
+import Control.Monad
+
+{-- IO union-find tree --}
+type UnionFindIO = IOArray Int (Int,Int)
+ 
+newUFIO :: Int -> IO UnionFindIO
+newUFIO n = newListArray (1,n) (zip [1..n] (repeat 1))
+ 
+findUFIO :: UnionFindIO -> Int -> IO Int
+findUFIO uf x = do
+  (parent,size) <- readArray uf x
+  if parent == x then
+    return x
+    else do
+    next <- findUFIO uf parent
+    writeArray uf x (next,size)
+    return next
+ 
+sameUFIO :: UnionFindIO -> Int -> Int -> IO Bool
+sameUFIO uf x y = do
+  xx <- findUFIO uf x
+  yy <- findUFIO uf y
+  return $ xx == yy
+ 
+uniteUFIO :: UnionFindIO -> Int -> Int -> IO ()
+uniteUFIO uf x y = do
+  xx <- findUFIO uf x
+  yy <- findUFIO uf y
+  (_,xs) <- readArray uf xx
+  (_,ys) <- readArray uf yy
+
+  if xx == yy then
+    return ()
+    else if xs < ys then do
+    writeArray uf xx (yy,0)
+    else if xs > ys then do
+    writeArray uf yy (xx,0)
+    else do
+    writeArray uf yy (xx,0)
+    writeArray uf xx (xx,xs+1)
