ace09021e4eb183b8303005928dbe48192db8cf1
Haar-you <zemlya.pole@gmail.com>
2018-09-19 14:21:08 +0900

    add 重み付きUnionFind

--- misc/Haskell/UnionFind.hs
+++ misc/Haskell/UnionFind.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiWayIf #-}
 
 import Data.Array.IO
 import Data.Array.ST
@@ -7,52 +8,112 @@ import Control.Monad.ST
 import Control.Applicative
 import qualified Data.Vector.Unboxed.Mutable as VUM
 import qualified Data.Vector.Unboxed as VU
+import Data.Maybe
 
-{-- IO union-find tree --}
-type IOUnionFind = STUnionFind RealWorld
-newIOUnionFind n = stToIO $ newSTUnionFind n
-findIOUnionFind uf x = stToIO $ findSTUnionFind uf x
-sameIOUnionFind uf x y = stToIO $ sameSTUnionFind uf x y 
-uniteIOUnionFind uf x y = stToIO $ uniteSTUnionFind uf x y
-sizeIOUnionFind uf x = stToIO $ sizeSTUnionFind uf x
-
-{-- ST union-find tree --}
 -- (親要素のIndex配列, 木の深さの配列, 木の要素数の配列)
-type STUnionFind s = (VUM.MVector s Int, VUM.MVector s Int, VUM.MVector s Int)
+type MUnionFind s = (VUM.MVector s Int, VUM.MVector s Int, VUM.MVector s Int)
+type IOUnionFind = MUnionFind RealWorld
+type STUnionFind s = MUnionFind s
 
-newSTUnionFind :: Int -> ST s (STUnionFind s)
-newSTUnionFind n = do
+newUnionFind :: (Applicative m, PrimMonad m) => Int -> m (MUnionFind (PrimState m))
+newUnionFind n = do
   parentList <- VU.thaw $ VU.fromList [0..n-1]
   depthList <- VU.thaw $ VU.fromList (take n (repeat 1))
   sizeList <- VU.thaw $ VU.fromList (take n (repeat 1))
   return (parentList, depthList, sizeList)
 
-findSTUnionFind :: STUnionFind s -> Int -> ST s Int
-findSTUnionFind uf@(plist,dlist,slist) x = do
-  parent <- VUM.read plist x
+findUnionFind :: (Applicative m, PrimMonad m) => MUnionFind (PrimState m) -> Int -> m Int
+findUnionFind uf@(plist,dlist,slist) x = do
+  parent <- VUM.unsafeRead plist x
   if parent == x then return x
-    else findSTUnionFind uf parent >>= \next -> VUM.write plist x next >> return next
+    else findUnionFind uf parent >>= \next -> VUM.unsafeWrite plist x next >> return next
 
-sameSTUnionFind :: STUnionFind s -> Int -> Int -> ST s Bool
-sameSTUnionFind uf x y = (==) <$> findSTUnionFind uf x <*> findSTUnionFind uf y
+sameUnionFind :: (Applicative m, PrimMonad m) => MUnionFind (PrimState m) -> Int -> Int -> m Bool
+sameUnionFind uf x y = (==) <$> findUnionFind uf x <*> findUnionFind uf y
   
-uniteSTUnionFind :: STUnionFind s -> Int -> Int -> ST s ()
-uniteSTUnionFind uf@(plist,dlist,slist) x y = do
-  xx <- findSTUnionFind uf x
-  yy <- findSTUnionFind uf y
-  xdepth <- VUM.read dlist xx
-  xsize <- VUM.read slist xx
-  ydepth <- VUM.read dlist yy
-  ysize <- VUM.read slist yy
+uniteUnionFind :: (Applicative m, PrimMonad m) => MUnionFind (PrimState m) -> Int -> Int -> m ()
+uniteUnionFind uf@(plist,dlist,slist) x y = do
+  xx <- findUnionFind uf x
+  yy <- findUnionFind uf y
+  xdepth <- VUM.unsafeRead dlist xx
+  xsize <- VUM.unsafeRead slist xx
+  ydepth <- VUM.unsafeRead dlist yy
+  ysize <- VUM.unsafeRead slist yy
 
   if xx == yy then return ()
-    else if xdepth < ydepth then VUM.write plist xx yy >> VUM.write dlist yy ydepth >> VUM.write slist yy (ysize+xsize)
-    else if xdepth > ydepth then VUM.write plist yy xx >> VUM.write dlist xx xdepth >> VUM.write slist xx (xsize+ysize)
-    else VUM.write plist yy xx >> VUM.write dlist xx (xdepth+1) >> VUM.write slist xx (xsize+ysize)
+    else if xdepth < ydepth then VUM.unsafeWrite plist xx yy >> VUM.unsafeWrite dlist yy ydepth >> VUM.unsafeWrite slist yy (ysize+xsize)
+    else if xdepth > ydepth then VUM.unsafeWrite plist yy xx >> VUM.unsafeWrite dlist xx xdepth >> VUM.unsafeWrite slist xx (xsize+ysize)
+    else VUM.unsafeWrite plist yy xx >> VUM.unsafeWrite dlist xx (xdepth+1) >> VUM.unsafeWrite slist xx (xsize+ysize)
 
-sizeSTUnionFind :: STUnionFind s -> Int -> ST s Int
-sizeSTUnionFind uf@(_,_,slist) x = VUM.read slist =<< findSTUnionFind uf x
+sizeUnionFind :: (Applicative m, PrimMonad m) => MUnionFind (PrimState m) -> Int -> m Int
+sizeUnionFind uf@(_,_,slist) x = VUM.unsafeRead slist =<< findUnionFind uf x
 
+
+-- (親要素のIndex配列, 木の深さの配列, 木の要素数の配列, 親要素との重さ差分の配列)
+type MWeightedUnionFind s = (VUM.MVector s Int, VUM.MVector s Int, VUM.MVector s Int, VUM.MVector s Int)
+type IOWeightedUnionFind = MWeightedUnionFind RealWorld
+type STWeightedUnionFind s = MWeightedUnionFind s
+
+newWeightedUnionFind :: (Applicative m, PrimMonad m) => Int -> m (MWeightedUnionFind (PrimState m))
+newWeightedUnionFind n = do
+  parentList <- VU.thaw $ VU.fromList [0..n-1]
+  depthList <- VU.thaw $ VU.fromList (take n (repeat 1))
+  sizeList <- VU.thaw $ VU.fromList (take n (repeat 1))
+  weightList <- VU.thaw $ VU.fromList (take n (repeat 0))
+  return (parentList, depthList, sizeList, weightList)
+
+findWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> m Int
+findWeightedUnionFind uf@(plist,dlist,slist,wlist) x = do
+  parent <- VUM.unsafeRead plist x
+  if parent == x then return x
+    else do
+    next <- findWeightedUnionFind uf parent
+    VUM.unsafeWrite wlist x =<< (+) <$> VUM.unsafeRead wlist x <*> VUM.unsafeRead wlist parent
+    VUM.unsafeWrite plist x next >> return next
+
+weighWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> m Int
+weighWeightedUnionFind uf@(_,_,_,wlist) x = findWeightedUnionFind uf x >> VUM.unsafeRead wlist x
+
+diffWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> Int -> m (Maybe Int)
+diffWeightedUnionFind uf x y = do
+  same <- sameWeightedUnionFind uf x y
+  if same then do
+    a <- (-) <$> weighWeightedUnionFind uf x <*> weighWeightedUnionFind uf y
+    return $ Just a
+    else
+    return Nothing
+
+sameWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> Int -> m Bool
+sameWeightedUnionFind uf x y = (==) <$> findWeightedUnionFind uf x <*> findWeightedUnionFind uf y
+  
+uniteWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> Int -> Int -> m ()
+uniteWeightedUnionFind uf@(plist,dlist,slist,wlist) x y w = do
+  xx <- findWeightedUnionFind uf x
+  yy <- findWeightedUnionFind uf y
+  xdepth <- VUM.unsafeRead dlist xx
+  xsize <- VUM.unsafeRead slist xx
+  ydepth <- VUM.unsafeRead dlist yy
+  ysize <- VUM.unsafeRead slist yy
+  xweight <- VUM.unsafeRead wlist x
+  yweight <- VUM.unsafeRead wlist y
+
+  if xx == yy then return ()
+    else
+    if
+      | xdepth < ydepth -> do
+          VUM.unsafeWrite plist xx yy >> VUM.unsafeWrite dlist yy ydepth >> VUM.unsafeWrite slist yy (ysize+xsize)
+          VUM.unsafeWrite wlist xx (w-xweight+yweight)
+
+      | xdepth > ydepth -> do
+          VUM.unsafeWrite plist yy xx >> VUM.unsafeWrite dlist xx xdepth >> VUM.unsafeWrite slist xx (xsize+ysize)
+          VUM.unsafeWrite wlist yy (-w+xweight-yweight)
+          
+      | xdepth == ydepth -> do
+          VUM.unsafeWrite plist yy xx >> VUM.unsafeWrite dlist xx (xdepth+1) >> VUM.unsafeWrite slist xx (xsize+ysize)
+          VUM.unsafeWrite wlist yy (-w+xweight-yweight)
+
+sizeWeightedUnionFind :: (Applicative m, PrimMonad m) => MWeightedUnionFind (PrimState m) -> Int -> m Int
+sizeWeightedUnionFind uf@(_,_,slist,_) x = VUM.unsafeRead slist =<< findWeightedUnionFind uf x
 
 main :: IO ()
 main = do
