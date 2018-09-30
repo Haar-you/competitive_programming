9d9ce029f4fa7b30bc75059e55298dbd4013a7fd
Haar-you <zemlya.pole@gmail.com>
2018-10-01 03:04:29 +0900

    add 素数/Fibonacci数/中国剰余定理

--- misc/Haskell/Calculation.hs
+++ misc/Haskell/Calculation.hs
@@ -1,4 +1,4 @@
-import Data.Ratio
+ =import Data.Ratio
 import Data.List
 
 
@@ -60,6 +60,39 @@ powerMod n 1 m = n `mod` m
 powerMod n p m = (k * k * (if mod p 2 == 0 then 1 else n)) `mod` m
   where
     k = powerMod n (div p 2) m
+
+
+{-- sieve of eratosthenes--}
+primes = aux [2..]
+  where aux (x:xs) = x : aux [y | y<-xs, y`mod`x/=0]
+
+
+{-- Fibonacci数の一般項 (mod p)--}
+fibonacci 0 _ = 0
+fibonacci 1 _ = 1
+fibonacci n p = let [x,_,_,_] = matpow [1,1,1,0] (n-1) in x `mod` p
+  where
+    matprod [a,b,c,d] [w,x,y,z] = map (`mod` p) [a*w+b*y,a*x+b*z,c*w+d*y,c*x+d*z]
+    matpow a 0 = [1,0,0,1]
+    matpow a 1 = a
+    matpow a p = let t = matpow a (div p 2) in (if even p then id else (`matprod` a)) (t `matprod` t)
+
+{-- 拡張Euclid互除法 --}
+extendedEuclidean :: Integral a => a -> a -> (a, a, a)
+extendedEuclidean a 0 = (a,1,0)
+extendedEuclidean a b = (d,p,q-a`div`b*p)
+  where (d,q,p) = extendedEuclidean b (a`mod`b)
+
+{-- 中国剰余定理 --}
+chineseRemainder :: Integral a => [(a,a)] -> Maybe (a,a)
+chineseRemainder xs = foldl aux (Just (0,1)) xs
+  where
+    aux Nothing _ = Nothing
+    aux (Just (b1,m1)) (b2,m2)
+      | (b2-b1) `mod` d /= 0 = Nothing
+      | otherwise = Just (b1+m1*(((b2-b1)*p`div`d)`mod`(m2`div`d)), m1*m2`div`d)
+      where
+        (d,p,q) = extendedEuclidean m1 m2
 
 
 main :: IO ()
