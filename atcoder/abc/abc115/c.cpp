43f03bf60c06ad864ac93a09389045af29515b96
Haar-you <zemlya.pole@gmail.com>
2018-12-08 23:17:20 +0900

    ABC115

---
+++ atcoder/abc/abc115/c.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define FOR(v, a, b) for(int v = (a); v < (b); ++v)
+#define FORE(v, a, b) for(int v = (a); v <= (b); ++v)
+#define REP(v, n) FOR(v, 0, n)
+#define REPE(v, n) FORE(v, 0, n)
+#define REV(v, a, b) for(int v = (a); v >= (b); --v)
+#define ALL(x) (x).begin(), (x).end()
+#define ITR(it, c) for(auto it = (c).begin(); it != (c).end(); ++it)
+#define LLI long long int
+using namespace std;
+template <typename T> using V = vector<T>;
+template <typename T, typename U> using P = pair<T,U>;
+template <typename I> void join(ostream &ost, I s, I t, string d=" "){for(auto i=s; i!=t; ++i){if(i!=s)ost<<d; ost<<*i;}ost<<endl;}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  int n,k; cin >> n >> k;
+
+  vector<int> h(n);
+  REP(i,n) cin >> h[i];
+
+  sort(ALL(h));
+
+  int ans = 1e9;
+
+  REPE(i,n-k){
+    ans = min(ans, h[i+k-1] - h[i]);
+  }
+  
+  cout << ans << endl;
+  
+  return 0;
+}
