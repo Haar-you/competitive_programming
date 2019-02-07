e041669362fb0aedadb2b8b5fa5be487ad1c7bde
Haar-you <zemlya.pole@gmail.com>
2019-02-08 06:48:02 +0900

    Codeforces Global Round 1

---
+++ codeforces/1110/a.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define FOR(v, a, b) for(int v = (a); v < (b); ++v)
+#define FORE(v, a, b) for(int v = (a); v <= (b); ++v)
+#define REP(v, n) FOR(v, 0, n)
+#define REPE(v, n) FORE(v, 0, n)
+#define REV(v, a, b) for(int v = (a); v >= (b); --v)
+#define ALL(x) (x).begin(), (x).end()
+#define ITR(it, c) for(auto it = (c).begin(); it != (c).end(); ++it)
+#define RITR(it, c) for(auto it = (c).rbegin(); it != (c).rend(); ++it)
+#define EXIST(c,x) ((c).find(x) != (c).end())
+#define LLI long long int
+#define fst first
+#define snd second
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_E
+#define M_E 2.71828182845904523536
+#endif
+#ifdef DEBUG
+#include <boost/core/demangle.hpp>
+#define dump(x) cerr << "L" << __LINE__ << ": in " << __PRETTY_FUNCTION__ << " \e[32;1m" << boost::core::demangle(typeid(x).name()) << "\e[37m" << " " << (#x) << " = " << (x) << "\e[m" << endl;
+#else
+#define dump(x)
+#endif
+#define pln(x) cout << (x) << endl
+#define gcd __gcd
+
+using namespace std;
+template <class T> constexpr T lcm(T m, T n){return m/gcd(m,n)*n;}
+
+template <typename T> using V = vector<T>;
+template <typename T, typename U> using P = pair<T,U>;
+template <typename I> void join(ostream &ost, I s, I t, string d=" "){for(auto i=s; i!=t; ++i){if(i!=s)ost<<d; ost<<*i;}ost<<endl;}
+template <typename T> istream& operator>>(istream &is, vector<T> &v){for(auto &a : v) is >> a; return is;}
+template <typename T, typename U> istream& operator>>(istream &is, pair<T,U> &p){is >> p.first >> p.second; return is;}
+
+template <typename Iter> ostream& out_container(ostream &os, Iter first, Iter last){
+  os << "{"; auto itr = first; while(itr != last){if(itr != first) os << ","; os << *itr; ++itr;} os << "}"; return os;
+}
+
+template <typename T> ostream& operator<<(ostream &os, const vector<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const deque<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const set<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const unordered_set<T> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream &os, const map<T,U> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream &os, const unordered_map<T,U> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T,U> &p){os << "(" << p.first << "," << p.second << ")"; return os;}
+
+template <typename T, typename U> T& chmin(T &a, const U &b){return a = (a<=b?a:b);}
+template <typename T, typename U> T& chmax(T &a, const U &b){return a = (a>=b?a:b);}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  LLI b,k;
+  while(cin >> b >> k){
+    vector<LLI> a(k); cin >> a;
+
+    LLI ans = 0;
+    REP(i,k){
+      (ans *= b) %= 2;
+      (ans += a[i]) %= 2;
+    }
+
+    cout << (ans ? "odd" : "even") << endl;
+  
+  }
+  
+  return 0;
+}
---
+++ codeforces/1110/c.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+#define FOR(v, a, b) for(int v = (a); v < (b); ++v)
+#define FORE(v, a, b) for(int v = (a); v <= (b); ++v)
+#define REP(v, n) FOR(v, 0, n)
+#define REPE(v, n) FORE(v, 0, n)
+#define REV(v, a, b) for(int v = (a); v >= (b); --v)
+#define ALL(x) (x).begin(), (x).end()
+#define ITR(it, c) for(auto it = (c).begin(); it != (c).end(); ++it)
+#define RITR(it, c) for(auto it = (c).rbegin(); it != (c).rend(); ++it)
+#define EXIST(c,x) ((c).find(x) != (c).end())
+#define LLI long long int
+#define fst first
+#define snd second
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+#ifndef M_E
+#define M_E 2.71828182845904523536
+#endif
+#ifdef DEBUG
+#include <boost/core/demangle.hpp>
+#define dump(x) cerr << "L" << __LINE__ << ": in " << __PRETTY_FUNCTION__ << " \e[32;1m" << boost::core::demangle(typeid(x).name()) << "\e[37m" << " " << (#x) << " = " << (x) << "\e[m" << endl;
+#else
+#define dump(x)
+#endif
+#define pln(x) cout << (x) << endl
+#define gcd __gcd
+
+using namespace std;
+template <class T> constexpr T lcm(T m, T n){return m/gcd(m,n)*n;}
+
+template <typename T> using V = vector<T>;
+template <typename T, typename U> using P = pair<T,U>;
+template <typename I> void join(ostream &ost, I s, I t, string d=" "){for(auto i=s; i!=t; ++i){if(i!=s)ost<<d; ost<<*i;}ost<<endl;}
+template <typename T> istream& operator>>(istream &is, vector<T> &v){for(auto &a : v) is >> a; return is;}
+template <typename T, typename U> istream& operator>>(istream &is, pair<T,U> &p){is >> p.first >> p.second; return is;}
+
+template <typename Iter> ostream& out_container(ostream &os, Iter first, Iter last){
+  os << "{"; auto itr = first; while(itr != last){if(itr != first) os << ","; os << *itr; ++itr;} os << "}"; return os;
+}
+
+template <typename T> ostream& operator<<(ostream &os, const vector<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const deque<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const set<T> &c){return out_container(os,ALL(c));}
+template <typename T> ostream& operator<<(ostream &os, const unordered_set<T> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream &os, const map<T,U> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream &os, const unordered_map<T,U> &c){return out_container(os,ALL(c));}
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T,U> &p){os << "(" << p.first << "," << p.second << ")"; return os;}
+
+template <typename T, typename U> T& chmin(T &a, const U &b){return a = (a<=b?a:b);}
+template <typename T, typename U> T& chmax(T &a, const U &b){return a = (a>=b?a:b);}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+  
+  vector<int> t = {0,1,1,5,1,21,1,85,73,341,89,1365,1,5461,4681,21845,1,87381,1,349525,299593,1398101,178481,
+		   5592405,1082401,22369621};
+
+  map<int,int> m;
+  {
+    int a = 1;
+    REPE(i,25){
+      int r = 0;
+      m[a] = t[i];
+      a = (a<<1) + 1;
+    }
+  }
+
+  int q;
+  while(cin >> q){
+    REP(_,q){
+      int a; cin >> a;
+
+      if(EXIST(m,a)){
+	cout << m[a] << endl;
+      }else{
+	int b = 1;
+	while(b <= a){
+	  b = (b<<1);
+	}
+	cout << b-1 << endl;
+      }
+    }
+  }
+  
+  return 0;
+}
