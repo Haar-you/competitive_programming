b51d9f739c6925ce3d2fd4122fc2e7a73597ef4c
Haar-you <zemlya.pole@gmail.com>
2019-04-25 02:42:34 +0900

    Codeforces Round #554 (Div. 2)

---
+++ codeforces/1152/c.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#define FOR(v, a, b) for(int v = (a); v < (b); ++v)
+#define FORE(v, a, b) for(int v = (a); v <= (b); ++v)
+#define REP(v, n) FOR(v, 0, n)
+#define REPE(v, n) FORE(v, 0, n)
+#define REV(v, a, b) for(int v = (a); v >= (b); --v)
+#define ALL(x) (x).begin(), (x).end()
+#define RALL(x) (x).rbegin(), (x).rend()
+#define ITR(it, c) for(auto it = (c).begin(); it != (c).end(); ++it)
+#define RITR(it, c) for(auto it = (c).rbegin(); it != (c).rend(); ++it)
+#define EXIST(c,x) ((c).find(x) != (c).end())
+#define LLI long long int
+#define fst first
+#define snd second
+
+#ifdef DEBUG
+#include <misc/C++/Debug.cpp>
+#else
+#define dump(x) ((void)0)
+#endif
+
+#define gcd __gcd
+
+using namespace std;
+template <class T> constexpr T lcm(T m, T n){return m/gcd(m,n)*n;}
+
+template <typename I> void join(ostream &ost, I s, I t, string d=" "){for(auto i=s; i!=t; ++i){if(i!=s)ost<<d; ost<<*i;}ost<<endl;}
+template <typename T> istream& operator>>(istream &is, vector<T> &v){for(auto &a : v) is >> a; return is;}
+template <typename T, typename U> istream& operator>>(istream &is, pair<T,U> &p){is >> p.first >> p.second; return is;}
+
+template <typename T, typename U> bool chmin(T &a, const U &b){return (a>b ? a=b, true : false);}
+template <typename T, typename U> bool chmax(T &a, const U &b){return (a<b ? a=b, true : false);}
+template <typename T, size_t N, typename U> void fill_array(T (&a)[N], const U &v){fill((U*)a, (U*)(a+N), v);}
+
+template <typename T> bool is_square(T n){T rt = sqrt(n); return rt*rt == n;}
+
+vector<LLI> divisor_list(LLI n){
+  vector<LLI> temp, res;
+  for(LLI i=1LL; i*i<n; ++i) if(n%i == 0) temp.push_back(i);
+  for(auto i : temp) res.push_back(i);
+  if(is_square(n)) res.push_back((LLI)sqrt(n));
+  for(auto itr=temp.rbegin(); itr!=temp.rend(); ++itr) res.push_back(n/(*itr));
+  return res;
+}
+
+int main(){
+  cin.tie(0);
+  ios::sync_with_stdio(false);
+
+  LLI a,b;
+  while(cin >> a >> b){
+
+    LLI ans = LLONG_MAX;
+
+    if(a==b){
+      ans = 0;
+    }else{
+      LLI d = abs(a-b);
+
+      auto divs = divisor_list(d);
+
+      LLI l = lcm(a,b);
+      ans = 0;
+      
+      for(auto x : divs){
+	LLI k = min(x-a, x-b);
+
+	if(k < 0){
+	  k += x*(((-k)-1)/x+1);
+	}
+	
+	if(a+k <= l and b+k <= l){
+	  if(lcm(a+k,b+k) < l){
+	    ans = k;
+	    chmin(l, lcm(a+k,b+k));
+	  }else if(lcm(a+b,b+k) == l){
+	    chmin(ans, k);
+	  }
+	}
+      }
+    }
+
+    cout << ans << endl;
+  }
+  
+  return 0;
+}
