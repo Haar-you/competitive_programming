35066da1773f1c8e14d846eb9986191f43157d4d
Haar-you <zemlya.pole@gmail.com>
2019-05-26 15:11:32 +0900

    dumpの複数引数/見た目の変更/実行時間計測を表示

--- misc/C++/Debug.cpp
+++ misc/C++/Debug.cpp
@@ -1,86 +1,222 @@
 //#include<bits/stdc++.h>
-using namespace std;
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <boost/format.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 
 #include <boost/core/demangle.hpp>
 
-#define dump(x) dump_aux(__LINE__, __PRETTY_FUNCTION__, (#x), x)
+using namespace std;
 
-template <typename T> void dump_aux(const int line, const char *func, const char *name, const T &val){
-  cerr << "L" << line;
-  cerr << ": in" << func;
-  cerr << " \e[32;1m" << boost::core::demangle(typeid(val).name()) << "\e[37m" << " " << name << " = " << val << "\e[m" << endl;
+
+template <typename T> void dump_out(const T &val);
+
+
+#define dump(...) dump_aux(__LINE__, __PRETTY_FUNCTION__, (#__VA_ARGS__), __VA_ARGS__)
+
+template <class T> void dump_show_name_val(const string &name, const T val){
+  cerr << "    \e[32;1m" << boost::core::demangle(typeid(val).name()) << "\e[37m" << " " << name << " = ";
+  dump_out(val);
+  cerr << "\e[m" << endl;
 }
 
-template <typename Iter> ostream& out_container(ostream &os, Iter first, Iter last){
-  os << "{";
+template <class T> void dump_aux_aux(vector<string> &names, const T &val){
+  dump_show_name_val(names.back(), val);
+}
+
+template <class T, class... U> void dump_aux_aux(vector<string> &names, const T &val, U const &... vals){
+  dump_show_name_val(names.back(), val);
+  names.pop_back();
+
+  dump_aux_aux(names, vals...);
+}
+
+template <class... T> void dump_aux(const int line, const char *func, const char *name, T const &... vals){
+  cerr << "\e[47;30m" << "L" << line <<  ": in " << func << "\e[m" << endl;
+
+  vector<string> valnames;
+  boost::algorithm::split(valnames, name, boost::is_any_of(","));
+  reverse(ALL(valnames));
+
+  dump_aux_aux(valnames, vals...);
+}
+
+template <typename T> void dump_out_aux(const vector<T> &val);
+template <typename T, size_t N> void dump_out_aux(const array<T,N> &val);
+template <typename T> void dump_out_aux(const deque<T> &val);
+template <typename T> void dump_out_aux(const stack<T> &val);
+template <typename T> void dump_out_aux(const queue<T> &val);
+template <typename T> void dump_out_aux(const priority_queue<T> &val);
+template <typename T, typename Container, typename Compare> void dump_out_aux(const priority_queue<T,Container,Compare> &val);
+template <typename t> void dump_out_aux(const set<t> &val);
+template <typename T> void dump_out_aux(const unordered_set<T> &val);
+template <typename T, typename U> void dump_out_aux(const map<T,U> &val);
+template <typename T, typename U> void dump_out_aux(const unordered_map<T,U> &val);
+
+template <typename T, typename U> void dump_out_aux(const pair<T,U> &val);
+template <typename T1, typename T2, typename T3> void dump_out_aux(const tuple<T1,T2,T3> &val);
+template <typename T1, typename T2, typename T3, typename T4> void dump_out_aux(const tuple<T1,T2,T3,T3> &val);
+template <typename T1, typename T2, typename T3, typename T4, typename T5> void dump_out_aux(const tuple<T1,T2,T3,T4,T5> &val);
+
+void dump_out_aux(int val);
+void dump_out_aux(double val);
+void dump_out_aux(const string &val);
+
+
+
+
+template <typename Iter> void out_container(Iter first, Iter last){
+  cerr << "{";
   auto itr = first;
   while(itr != last){
-    if(itr != first) os << ",";
-    os << *itr;
+    if(itr != first) cerr << ",";
+    dump_out_aux(*itr);
     ++itr;
   }
-  os << "}";
-  return os;
+  cerr << "}";
 }
 
-// vector / array
-template <typename T> ostream& operator<<(ostream &os, const vector<T> &c){return out_container(os,ALL(c));}
-template <typename T, size_t N> ostream& operator<<(ostream &os, const array<T,N> &c){return out_container(os,ALL(c));}
+template <typename T> void show_size(const T &val){
+  cerr << ": " << "size=" << val.size();
+}
+
+// vector
+template <typename T> void dump_out_aux(const vector<T> &val){
+  out_container(ALL(val));
+  show_size(val);
+}
+
+// array
+template <typename T, size_t N> void dump_out_aux(const array<T,N> &val){
+  out_container(ALL(val));
+  show_size(val);
+}
 
 // deque
-template <typename T> ostream& operator<<(ostream &os, const deque<T> &c){return out_container(os,ALL(c));}
+template <typename T> void dump_out_aux(const deque<T> &val){
+  out_container(ALL(val));
+  show_size(val);
+}
 
 // stack
-template <typename T>
-ostream& operator<<(ostream &os, const stack<T> &s){
-  if(s.empty()) os << "{}";
-  else os << "{" << s.top() << ",...}";
-  return os;
+template <typename T> void dump_out_aux(const stack<T> &val){
+  if(val.empty()) cerr << "{}";
+  else if(val.size() == 1) cerr << "{" << val.top() << "}";
+  else cerr << "{" << val.top() << ", ...}";
+  show_size(val);
 }
 
 // queue
-template <typename T>
-ostream& operator<<(ostream &os, const queue<T> &q){
-  if(q.empty()) os << "{}";
-  else os << "{" << q.front() << ",...," << q.back() << "}";
-  return os;
+template <typename T> void dump_out_aux(const queue<T> &val){
+  if(val.empty()) cerr << "{}";
+  else if(val.size() == 1) cerr << "{" << val.front() << "}";
+  else if(val.size() == 2) cerr << "{" << val.front() << "," << val.back() << "}"; 
+  else cerr << "{" << val.front() << ", ..., " << val.back() << "}";
+  show_size(val);
 }
 
 // priority_queue
-template <typename T>
-ostream& operator<<(ostream &os, const priority_queue<T> &pq){
-  if(pq.empty()) os << "{}";
-  else os << "{" << pq.top() << ",...}";
-  return os;
+template <typename T> void dump_out_aux(const priority_queue<T> &val){
+  if(val.empty()) cerr << "{}";
+  else if(val.size() == 1) cerr << "{" << val.top() << "}";
+  else cerr << "{" << val.top() << ", ..." << "}";
+  show_size(val);
 }
+
+template <typename T, typename Container, typename Compare> void dump_out_aux(const priority_queue<T,Container,Compare> &val){
+  if(val.empty()) cerr << "{}";
+  else if(val.size() == 1) cerr << "{" << val.top() << "}";
+  else cerr << "{" << val.top() << ", ..." << "}";
+  show_size(val);
+}
+
+
 
 
 // set
-template <typename T> ostream& operator<<(ostream &os, const set<T> &c){return out_container(os,ALL(c));}
-template <typename T> ostream& operator<<(ostream &os, const unordered_set<T> &c){return out_container(os,ALL(c));}
+template <typename T> void dump_out_aux(const set<T> &val){
+  out_container(ALL(val));
+  show_size(val);
+}
+
+// unrodered_set
+template <typename T> void dump_out_aux(const unordered_set<T> &val){
+  out_container(ALL(val));
+  show_size(val);
+}
 
 // map
-template <typename T, typename U> ostream& operator<<(ostream &os, const map<T,U> &c){return out_container(os,ALL(c));}
-template <typename T, typename U> ostream& operator<<(ostream &os, const unordered_map<T,U> &c){return out_container(os,ALL(c));}
-
-
-// pair / tuple
-template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T,U> &p){os << "(" << p.first << "," << p.second << ")"; return os;}
-
-template <typename T1, typename T2, typename T3>
-ostream& operator<<(ostream &os, const tuple<T1,T2,T3> &t){
-  os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << ")";
-  return os;
+template <typename T, typename U> void dump_out_aux(const map<T,U> &val){
+  out_container(ALL(val));
+  show_size(val); 
 }
 
-template <typename T1, typename T2, typename T3, typename T4>
-ostream& operator<<(ostream &os, const tuple<T1,T2,T3,T4> &t){
-  os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << "," << get<3>(t) << ")";
-  return os;
+// unordered_map
+template <typename T, typename U> void dump_out_aux(const unordered_map<T,U> &val){
+  out_container(ALL(val));
+  show_size(val);
 }
 
-template <typename T1, typename T2, typename T3, typename T4, typename T5>
-ostream& operator<<(ostream &os, const tuple<T1,T2,T3,T4,T5> &t){
-  os << "(" << get<0>(t) << "," << get<1>(t) << "," << get<2>(t) << "," << get<3>(t) << "," << get<4>(t) << ")";
-  return os;
+
+// pair
+template <typename T, typename U> void dump_out_aux(const pair<T,U> &val){
+  cerr << "(" << val.fst << "," << val.snd << ")";
 }
+
+// tuple (3~5)
+template <typename T1, typename T2, typename T3> void dump_out_aux(const tuple<T1,T2,T3> &val){
+  cerr << "(" << get<0>(val) << "," << get<1>(val) << "," << get<2>(val) << ")";
+}
+
+template <typename T1, typename T2, typename T3, typename T4> void dump_out_aux(const tuple<T1,T2,T3,T3> &val){
+  cerr << "(" << get<0>(val) << "," << get<1>(val) << "," << get<2>(val) << "," << get<3>(val) << ")";
+}
+
+template <typename T1, typename T2, typename T3, typename T4, typename T5> void dump_out_aux(const tuple<T1,T2,T3,T4,T5> &val){
+  cerr << "(" << get<0>(val) << "," << get<1>(val) << "," << get<2>(val) << "," << get<3>(val) << "," << get<4>(val) << ")";
+}
+
+// others
+void dump_out_aux(int val){
+  cerr << val;
+}
+
+void dump_out_aux(double val){
+  cerr << val;
+}
+
+void dump_out_aux(const string &val){
+  cerr << val;
+}
+
+
+template <typename T> void dump_out_aux(const T &val){
+  cerr << val;
+}
+
+
+template <typename T> void dump_out(const T &val){
+  dump_out_aux(val);
+}
+
+
+/*
+
+
+ */
+
+struct PreMain{
+  chrono::system_clock::time_point start, end;
+  
+  PreMain(){
+    start = chrono::system_clock::now();
+  }
+  ~PreMain(){
+    end = chrono::system_clock::now();
+    
+    int64_t exec_time = static_cast<int64_t>(chrono::duration_cast<chrono::milliseconds>(end - start).count());
+    cerr << "\e[33;01m" << exec_time << "ms" << "\e[m" << endl;
+  }
+}PreMain;
