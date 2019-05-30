37ad8ad2847ed1dd98c5cdaf4bad12c2deada763
Haar-you <zemlya.pole@gmail.com>
2019-05-30 21:55:56 +0900

    optional/variant/any/bool/charのdumpを実装

--- misc/C++/Debug.cpp
+++ misc/C++/Debug.cpp
@@ -36,9 +36,17 @@ template <typename T, typename U> void dump_out_aux(const unordered_multimap<T,U
 
 template <typename T> void dump_out_aux(const complex<T> &val);
 
+#if __cplusplus == 201703L
+template <typename T> void dump_out_aux(const optional<T> &val);
+template <typename... T> void dump_out_aux(const variant<T...> &val);
+void dump_out_aux(const any &val);
+#endif
+
+void dump_out_aux(bool val);
 void dump_out_aux(int val);
 void dump_out_aux(double val);
 void dump_out_aux(const string &val);
+void dump_out_aux(char val);
 
 template <typename T, size_t N> void dump_out_aux(const T (&val)[N]);
 
@@ -281,13 +289,66 @@ template <typename T> void dump_out_aux(const complex<T> &val){
   cerr << val.real() << (val.imag() < 0 ? "" : "+") << val.imag() << "i";
 }
 
+#if __cplusplus == 201703L
+// optional
+template <typename T> void dump_out_aux(const optional<T> &val){
+  if(val.has_value()){
+    dump_out_aux(val.value());
+  }else{
+    cerr << "none";
+  }
+}
 
+// variant
+template <typename T, size_t N>
+struct VariantPrintHelper{
+  static void print(const T &val){
+    auto p = get_if<N-1>(&val);
+    if(p){
+      cerr << "\e[32m<" << boost::core::demangle(typeid(variant_alternative_t<N-1,T>).name()) << ">\e[37m";
+      dump_out_aux(*p);
+    }else{
+      VariantPrintHelper<T,N-1>::print(val);
+    }
+  }
+};
 
+template <typename T>
+struct VariantPrintHelper<T,1>{
+  static void print(const T &val){
+    auto p = get_if<0>(&val);
+    if(p){
+      cerr << "\e[32m<" << boost::core::demangle(typeid(variant_alternative_t<0,T>).name()) << ">\e[37m";
+      dump_out_aux(*p);
+    }
+  }
+};
 
+template <typename... T> void dump_out_aux(const variant<T...> &val){
+  VariantPrintHelper<variant<T...>, sizeof...(T)>::print(val);
+}
 
+// any
+void dump_out_aux(const any &val){
+  if(val.has_value()){
+    cerr << "has_value";
+  }else{
+    cerr << "none";
+  }
+}
+
+#endif
 
 
 // others
+void dump_out_aux(bool val){
+  if(val){
+    cerr << "True";
+  }else{
+    cerr << "False";
+  }
+}
+
 void dump_out_aux(int val){
   cerr << val;
 }
@@ -300,6 +361,11 @@ void dump_out_aux(const string &val){
   cerr << "\"" << val << "\"";
   show_size(val);
 }
+
+void dump_out_aux(char val){
+  cerr << "'" << val << "'";
+}
+
 
 
 // array
@@ -331,7 +397,7 @@ struct PreMain{
   }
   ~PreMain(){
     end = chrono::system_clock::now();
-    
+
     int64_t exec_time = static_cast<int64_t>(chrono::duration_cast<chrono::milliseconds>(end - start).count());
     cerr << "\e[33;01m" << exec_time << "ms" << "\e[m" << endl;
   }
