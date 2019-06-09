38bf687be5ec29602604e91a5d3a21a4a9e632b6
Haar-you <zemlya.pole@gmail.com>
2019-06-10 06:49:25 +0900

    add runtime_mod_int

--- misc/C++/ModInt.cpp
+++ misc/C++/ModInt.cpp
@@ -38,3 +38,42 @@ public:
 template <int M> istream& operator>>(istream &is, mod_int_64<M> &a){is >> a.val; return is;}
 template <int M> ostream& operator<<(ostream &os, const mod_int_64<M> &a){ os << a.val; return os;}
 
+// 実行時mod整数
+
+class runtime_mod_int{
+public:
+  static uint64_t M;
+  
+  uint64_t val;
+  runtime_mod_int(): val(0){}
+  runtime_mod_int(int64_t n): val(n%M){}
+  
+  const runtime_mod_int operator+(const runtime_mod_int &a) const {return runtime_mod_int((val+a.val)%M);}
+  const runtime_mod_int operator-(const runtime_mod_int &a) const {return runtime_mod_int((val-a.val+M)%M);}
+  const runtime_mod_int operator*(const runtime_mod_int &a) const {return runtime_mod_int((val*a.val)%M);}
+  const runtime_mod_int operator/(const runtime_mod_int &a) const {return runtime_mod_int((val*a.mod_inv().val)%M);}
+  
+  const runtime_mod_int& operator=(const runtime_mod_int &a){val = a.val; return *this;}
+  const runtime_mod_int& operator+=(const runtime_mod_int &a){(val += a.val) %= M; return *this;}
+  const runtime_mod_int& operator-=(const runtime_mod_int &a){((val -= a.val) += M) %= M; return *this;}
+  const runtime_mod_int& operator*=(const runtime_mod_int &a){(val *= a.val) %= M; return *this;}
+  const runtime_mod_int& operator/=(const runtime_mod_int &a){(val *= a.mod_inv().val) %= M; return *this;}
+
+  const bool operator==(const runtime_mod_int &a) const {return val==a.val;}
+  const bool operator!=(const runtime_mod_int &a) const {return val!=a.val;}
+
+  const runtime_mod_int power(LLI p) const{
+    runtime_mod_int ret = 1, e = val;
+    for(; p; e *= e, p >>= 1) if(p&1) ret *= e;
+    return ret;
+  }
+  
+  const runtime_mod_int mod_inv() const{
+    return power(M-2);
+  }
+};
+
+uint64_t runtime_mod_int::M;
+
+istream& operator>>(istream &is, runtime_mod_int &a){is >> a.val; return is;}
+ostream& operator<<(ostream &os, const runtime_mod_int &a){ os << a.val; return os;}
