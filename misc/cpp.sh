68ddcbf150ab7e1ff3400bfa4dab6c1b2db23b84
Haar-you <zemlya.pole@gmail.com>
2019-06-28 13:49:46 +0900

    update cpp.sh

--- misc/cpp.sh
+++ misc/cpp.sh
@@ -4,14 +4,14 @@ BOOST_PATH=$HOME/boost/include
 COMPRO_PATH=$HOME/Documents/competetive_programming
 
 FLAG_E=false
-INPUT_PATH=""
+FLAG_D=false
 
 # オプションの解析
-while getopts i:eh OPT
+while getopts ehd OPT
 do
   case $OPT in
-    i) INPUT_PATH=$OPTARG;; # 入力ファイルの指定
     e) FLAG_E=true;; # 実行のみ (コンパイルをしない)
+    d) FLAG_D=true;; # DEBUG定数を設定しない
     h) ;; # help
     \?) ;;
   esac
@@ -19,24 +19,32 @@ done
 
 shift $((OPTIND - 1))
 
+CPP_FILE=$1
+INPUT_FILE=$2
+
 # コンパイル & 実行
 function func(){
 
   # -eオプションが指定されているときのみコンパイルを行う
   if ! $FLAG_E ; then
-     echo -e "\e[33;1m compiling ... \e[m" 1>&2
-     g++ $1 -std=c++1z -Wall -I$BOOST_PATH -I$COMPRO_PATH -DDEBUG -D_GLIBCXX_DEBUG -g -o a.out
+    echo -e "\e[33;1m compiling ... \e[m" 1>&2
 
-     if [ $? -gt 0 ] ; then
-       return
-     fi
+    if $FLAG_D ; then
+      g++ $1 -std=c++1z -Wall -I$BOOST_PATH -I$COMPRO_PATH -D_GLIBCXX_DEBUG -g -o a.out
+    else
+      g++ $1 -std=c++1z -Wall -I$BOOST_PATH -I$COMPRO_PATH -DDEBUG -D_GLIBCXX_DEBUG -g -o a.out
+    fi
 
-     echo -e "\e[33;1m compile successful \e[m" 1>&2
+    if [ $? -gt 0 ] ; then
+      return
+    fi
+
+    echo -e "\e[33;1m compile successful \e[m" 1>&2
   fi
 
-  # -iオプションで指定されたファイルを標準入力として渡す
-  if [ $INPUT_PATH ] ; then
-    ./a.out < $INPUT_PATH;
+  # 第2引数で指定されたファイルを標準入力として渡す
+  if [ $INPUT_FILE ] ; then
+    ./a.out < $INPUT_FILE;
   else
     ./a.out
   fi
@@ -50,8 +58,7 @@ do
 done
 echo -e "\e[m";
 
-
-func $1 | while read line
+func $CPP_FILE | while read line
 do
   echo -en "\e[35;1m>\e[m \e[1m";
   echo $line;
